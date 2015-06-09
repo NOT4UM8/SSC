438e2bbd8e22008422be306c390354d5ec415c92
Sugata Sarkar <sugatasarkar@hotmail.com>
2015-06-10 01:00:22 +0530

    Database & Login Process Added

---
+++ SSC/Controllers/AccountController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SSC.Models;
+
+namespace SSC.Controllers
+{
+    public class AccountController : Controller
+    {
+        //
+        // GET: /Account/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login(LoginInputModel loginInput)
+        {
+            AccountDataModel accountData = new AccountDataModel();
+            if (loginInput != null)
+            {
+                if (accountData.Login(loginInput) == true)
+                {
+                    return RedirectToAction("LoginSuccess","Account");
+                }
+            }
+            return View();
+        }
+
+        public ActionResult LoginSuccess()
+        {
+            return View();
+        }
+    }
+}
---
+++ SSC/Models/AccountModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SSC.Models
+{
+    public class AccountModel
+    {
+    }
+
+    public class LoginInputModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class AccountDataModel
+    {
+        public bool Login(LoginInputModel loginInput)
+        {
+            using (SSCEntities db = new SSCEntities())
+            {
+                try
+                {
+                    var result = (from dt in db.User
+                                  where dt.Email == loginInput.Email && dt.Password == loginInput.Password
+                                  select dt).FirstOrDefault();
+                    if (result != null)
+                    {
+                        return true;
+                    }
+                }
+                catch (Exception ex)
+                { 
+                    
+                }
+            }
+            return false;
+        }
+    }
+}
